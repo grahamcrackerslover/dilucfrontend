70e35674e9f5a53ee82c9842dd0d18efcfb18bf0
LasterAlex <sasha.ishukshin@gmail.com>
2023-12-16 22:58:49 +0200

    Added fail and success pages

---
+++ src/pages/fail.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <h3 style="color: whitesmoke;">Не удалось купить предмет с айди {{ invid }}.</h3>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'fail',
+    data() {
+        return {
+            invid: "",
+        }
+    },
+    created() {
+        this.invid = this.$route.query.invid;  
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/success.vue
+++ src/pages/success.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <h3 style="color: whitesmoke;">Спасибо за покупку, оставьте отзыв, если хотите. Ваш код для отзыва: {{ review_code }}</h3>
+    <h3 style="color: whitesmoke;">Спасибо за покупку предмета айди {{ invid }}, оставьте отзыв, если хотите. Ваш код для отзыва: {{ review_code }}</h3>
 </template>
 
 <script lang="ts">
@@ -10,11 +10,13 @@ export default {
     name: 'success',
     data() {
         return {
-            review_code: ""
+            review_code: "",
+            invid: "",
         }
     },
     created() {
         this.review_code = this.$route.query.review_code;  
+        this.invid = this.$route.query.invid;  
     }
 }
 </script>
